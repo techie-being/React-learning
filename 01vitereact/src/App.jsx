b843b13701a239d8ab6978964442fd0258fbc500
rakesh <014rakeshofficial@gmail.com>
2025-08-22 12:42:43 +0530

    01vitereact

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+
+import Chai1 from "./Chai"   //imports "Chai" component from chai.jsx     .
+function App() {
+  
+  return (
+    <Chai1/>
+  )
+}
+
+
+export default App  //export default app function to main .jsx  as same way as we expoerted chai component in app.jsx  .
+//in jsx we can only return one element ,so if any situation ocuurs when we have to return multiple elements then we can use either div tag or "<>"" known as fragements in react.
---
+++ 01vitereact/src/Chai.jsx
@@ -0,0 +1,25 @@
+function Chai1(){
+    return(
+        <h1>jai shree mahaprabhu </h1>
+    )
+       
+}
+export default Chai1
+
+//this function chai is a called components in react. 
+//it is a good habbit to declare a function/components name first letter should be capital.
+// even if we declare our component file name in capital it is better.
+
+
+/* ⚡ Inner Workflow Summary till now
+
+Browser loads index.html → finds #root.
+
+Vite runs main.jsx → mounts React app inside #root.
+
+App.jsx is rendered → acts as the main layout.
+
+Inside App, <Chai /> is called → inserts Chai’s <h1>.
+
+Final UI is injected into the DOM.   */
+
